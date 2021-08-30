94074837c55a3ba532215120d77c3b214a354011
Vladyslav Bochkovskyi <bochkovskyi.vladyslav@gmail.com>
2021-08-30 21:01:32 +0300

    sadsasa

--- utils/commander.js
+++ utils/commander.js
@@ -1,1 +1,10 @@
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
+program.parse(process.argv);
